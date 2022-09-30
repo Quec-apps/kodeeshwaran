7282341b2c21c007847fb6f84ea2c92e689c0580
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2022-09-30 17:14:44 +0530

    Add files via upload

---
+++ content/res/var/Technology/easy.js
@@ -0,0 +1,211 @@
+i = 0;
+
+i++;
+window["i-e-q-ma"+i] = "കമ്പ്യൂട്ടിംഗിൽ, 'CPU' ലെ 'P' എന്താണ്?";
+window["i-e-q-en"+i] = "In computing, for what does the 'P' in 'CPU' stand?";
+window["i-e-A"+i] = "Processing";
+window["i-e-B"+i] = "Polynomial";
+window["i-e-C"+i] = "Predictive";
+window["i-e-D"+i] = "Partitioning";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "യന്ത്രവൽകൃത വിമാനം കണ്ടുപിടിച്ചത് ഏത് സഹോദരങ്ങളാണ്?";
+window["i-e-q-en"+i] = "What brothers were pioneers of motorized flight?";
+window["i-e-A"+i] = "Montgolfier";
+window["i-e-B"+i] = "Wright";
+window["i-e-C"+i] = "Earp";
+window["i-e-D"+i] = "Grimm";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "വെബ് പേജുകൾ സൃഷ്ടിക്കുന്നതിനുള്ള സ്റ്റാൻഡേർഡ് ഭാഷ എന്താണ്?";
+window["i-e-q-en"+i] = "What is the standard language for creating web pages?";
+window["i-e-A"+i] = "BASIC";
+window["i-e-B"+i] = "HTML";
+window["i-e-C"+i] = "MATLAB";
+window["i-e-D"+i] = "COBOL";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "ഒരു റഡാർ എന്തിനുവേണ്ടിയാണ് ഉപയോഗിക്കുന്നത്?";
+window["i-e-q-en"+i] = "For what is a radar used?";
+window["i-e-A"+i] = "Measuring flux";
+window["i-e-B"+i] = "Detecting objects";
+window["i-e-C"+i] = "Amplifying sound";
+window["i-e-D"+i] = "Substituting lasers";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "കമ്പ്യൂട്ടറിന്റെ ആപ്ലിക്കേഷൻ സോഫ്റ്റ്വെയറിന്റെ ഉദാഹരണം തിരഞ്ഞെടുക്കുക";
+window["i-e-q-en"+i] = "Select the example of application software of computer";
+window["i-e-A"+i] = "Ms Word";
+window["i-e-B"+i] = "Ms Excel";
+window["i-e-C"+i] = "Both A and B";
+window["i-e-D"+i] = "MS-DOS";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "പ്രിന്ററിന്റെ ഗുണനിലവാരം എങ്ങനെയാണ് അളക്കുന്നത്?";
+window["i-e-q-en"+i] = "How the quality of printer is measured?";
+window["i-e-A"+i] = "Alphabet per strike";
+window["i-e-B"+i] = "Words per Inch";
+window["i-e-C"+i] = "Strike per Inch";
+window["i-e-D"+i] = "Dots per Inch";
+window["i-e-ans"+i] = 4;
+
+i++;
+window["i-e-q-ma"+i] = "ഒരു തരം വിവരങ്ങൾ മാത്രം ഉൾക്കൊള്ളുന്ന ഒരു ഡാറ്റാബേസിന്റെ ഭാഗം എന്താണ്?";
+window["i-e-q-en"+i] = "What is part of a database that holds only one type of information?";
+window["i-e-A"+i] = "Report";
+window["i-e-B"+i] = "Field";
+window["i-e-C"+i] = "Record";
+window["i-e-D"+i] = "File";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "'OS' കമ്പ്യൂട്ടർ ചുരുക്കെഴുത്ത് സാധാരണയായി അർത്ഥമാക്കുന്നത്?";
+window["i-e-q-en"+i] = "'OS' computer abbreviation usually means ?";
+window["i-e-A"+i] = "Order of Significance";
+window["i-e-B"+i] = "Open Software";
+window["i-e-C"+i] = "Operating System";
+window["i-e-D"+i] = "Optical Sensor";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "'.MOV' വിപുലീകരണം സാധാരണയായി ഏത് തരത്തിലുള്ള ഫയലിനെയാണ് സൂചിപ്പിക്കുന്നത്?";
+window["i-e-q-en"+i] = "'.MOV' extension refers usually to what kind of file?";
+window["i-e-A"+i] = "Image file";
+window["i-e-B"+i] = "Animation/movie file";
+window["i-e-C"+i] = "Audio file";
+window["i-e-D"+i] = "MS Office document";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "ഒരു ഹോം വീഡിയോ റെക്കോർഡറിന്റെ ഏറ്റവും സാധാരണമായ ഫോർമാറ്റ് VHS ആണ്. VHS എന്നതിന്റെ അർത്ഥം...?";
+window["i-e-q-en"+i] = "The most common format for a home video recorder is VHS. VHS stands for...?";
+window["i-e-A"+i] = "Video Home System";
+window["i-e-B"+i] = "Very high speed";
+window["i-e-C"+i] = "Video horizontal standard";
+window["i-e-D"+i] = "Voltage house standard";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "'.MPG' വിപുലീകരണം സാധാരണയായി ഏത് തരത്തിലുള്ള ഫയലിനെയാണ് സൂചിപ്പിക്കുന്നത്?";
+window["i-e-q-en"+i] = "'.MPG' extension refers usually to what kind of file?";
+window["i-e-A"+i] = "WordPerfect Document file";
+window["i-e-B"+i] = "MS Office document";
+window["i-e-C"+i] = "Animation/movie file";
+window["i-e-D"+i] = "Image file";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "പാന്തർ (10.3), ജാഗ്വാർ (10.2), പ്യൂമ (10.1), ചീറ്റ (10.0) എന്നിവ ഏതുതരം ഓപ്പറേറ്റിംഗ് സിസ്റ്റത്തിന്റെ ഉദാഹരണങ്ങളാണ്?";
+window["i-e-q-en"+i] = "Panther (10.3), Jaguar (10.2), Puma (10.1), and Cheetah (10.0) are examples of what kind of operating system?";
+window["i-e-A"+i] = "Windows OS";
+window["i-e-B"+i] = "Mac OS";
+window["i-e-C"+i] = "Linux OS";
+window["i-e-D"+i] = "Android OS";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "ഇനിപ്പറയുന്നവയിൽ ഏതാണ് ജനപ്രിയ കമ്പനിയായ ആപ്പിൾ അവ പുറത്തിറക്കിയ ശരിയായ ക്രമം?";
+window["i-e-q-en"+i] = "Which among the following is the correct order in which popular company Apple released them?";
+window["i-e-A"+i] = "Mac, iPod, iPad, iPhone";
+window["i-e-B"+i] = "Mac, iPhone, iPod, iPad";
+window["i-e-C"+i] = "Mac, iPod, iPhone, iPad";
+window["i-e-D"+i] = "Mac, iPad, ipod, iPhone";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "ഒരു കമ്പ്യൂട്ടറിന് ഏത് തരത്തിലുള്ള പ്രവർത്തനങ്ങൾ നടത്താൻ കഴിയും?";
+window["i-e-q-en"+i] = "What kind of operations can a computer carry out?";
+window["i-e-A"+i] = "Arithmetic and Logical";
+window["i-e-B"+i] = "Verbal and Logical";
+window["i-e-C"+i] = "Logical and Cognitive";
+window["i-e-D"+i] = "Verbal and Arithmetic";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "മോണിറ്ററുകൾ, പ്രിന്ററുകൾ, ഡിസ്ക് ഡ്രൈവുകൾ, കീബോർഡ്, മൗസ് എന്നിവ ഇനിപ്പറയുന്നവയിൽ ഏതൊക്കെ വിഭാഗത്തിലാണ് ഉൾപ്പെടുത്തിയിരിക്കുന്നത്?";
+window["i-e-q-en"+i] = "Monitors, printers, disk drives, keyboard, mouse are included in which of the following categories?";
+window["i-e-A"+i] = "Storage Device";
+window["i-e-B"+i] = "Hardware";
+window["i-e-C"+i] = "Software";
+window["i-e-D"+i] = "All of the Above";
+window["i-e-ans"+i] = 2;
+
+i++;
+window["i-e-q-ma"+i] = "കമ്പ്യൂട്ടർ സിസ്റ്റത്തിൽ സാധ്യമായ തകരാറുകൾ കണ്ടെത്തുന്നതിന് ഉപയോഗിക്കുന്ന പ്രോഗ്രാമുകൾക്ക് ഇനിപ്പറയുന്ന പദങ്ങളിൽ ഏതാണ് പലപ്പോഴും ഉപയോഗിക്കുന്നത്?";
+window["i-e-q-en"+i] = "Which of the following terms is often used for the programs that are used to find out possible faults in the computer system?";
+window["i-e-A"+i] = "Operating system extensions";
+window["i-e-B"+i] = "Cookies";
+window["i-e-C"+i] = "Diagnostic software";
+window["i-e-D"+i] = "Boot Diskettes";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "ഡാറ്റയുടെയോ പ്രോഗ്രാം ഫയലുകളുടെയോ പകർപ്പുകൾ എല്ലായ്പ്പോഴും ഒരു സുരക്ഷിതമായി സൂക്ഷിക്കുന്നു. ഇനിപ്പറയുന്നവയിൽ ഏതാണ് ഈ കോപ്പികൾക്ക് നൽകിയിരിക്കുന്ന പേര്?";
+window["i-e-q-en"+i] = "The copies of data or program files are always stored as a safety. Which of the following is the name given to these copies?";
+window["i-e-A"+i] = "Backups";
+window["i-e-B"+i] = "Programs";
+window["i-e-C"+i] = "Extras";
+window["i-e-D"+i] = "Stores";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "പേഴ്സണൽ കമ്പ്യൂട്ടറിന്റെ ഇനിപ്പറയുന്ന ഘടകങ്ങളിൽ ഏതാണ് സിസ്റ്റത്തിന്റെ തീയതിയും സമയവും പരിപാലിക്കുന്നത്?";
+window["i-e-q-en"+i] = "Which of the following components of Personal Computer maintains date and time of the system?";
+window["i-e-A"+i] = "CMOS RAM";
+window["i-e-B"+i] = "VRAM";
+window["i-e-C"+i] = "PROM";
+window["i-e-D"+i] = "Motherboard";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "ഇതിൽ ഏതാണ് ഇന്ത്യൻ സ്മാർട്ട്‌ഫോൺ നിർമ്മാതാവ്?";
+window["i-e-q-en"+i] = "Which of these is an Indian smartphone manufacturer?";
+window["i-e-A"+i] = "Karbonn";
+window["i-e-B"+i] = "Xiaomi";
+window["i-e-C"+i] = "Oppo";
+window["i-e-D"+i] = "Vivo";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "MS Windows 10-ൽ മുൻകൂട്ടി ഇൻസ്റ്റാൾ ചെയ്തിട്ടുള്ള വെബ് ബ്രൗസർ ഏതാണ്?";
+window["i-e-q-en"+i] = "Which web browser comes pre-installed with MS Windows 10?";
+window["i-e-A"+i] = "Edge";
+window["i-e-B"+i] = "Chrome";
+window["i-e-C"+i] = "Safari";
+window["i-e-D"+i] = "Firefox";
+window["i-e-ans"+i] = 1;
+
+i++;
+window["i-e-q-ma"+i] = "വിൻഡോസ് ഒഎസിനായി എംഎസ് എക്സൽ എപ്പോഴാണ് ആരംഭിച്ചത്?";
+window["i-e-q-en"+i] = "When was MS Excel launched for Windows OS?";
+window["i-e-A"+i] = "1986";
+window["i-e-B"+i] = "1985";
+window["i-e-C"+i] = "1987";
+window["i-e-D"+i] = "1988";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "കോൺഫറൻസുകൾക്കും മീറ്റിംഗുകൾക്കും തത്സമയം ഉപയോഗിക്കാനാകുന്ന Microsoft Office ആപ് ഏതാണ്?";
+window["i-e-q-en"+i] = "Which of these Microsoft Office apps can be used for conferences and meetings in real time?";
+window["i-e-A"+i] = "MS Powerpoint";
+window["i-e-B"+i] = "MS Excel";
+window["i-e-C"+i] = "Skype for Business";
+window["i-e-D"+i] = "MS Project";
+window["i-e-ans"+i] = 3;
+
+i++;
+window["i-e-q-ma"+i] = "MS PowerPoint-ലോ മറ്റേതെങ്കിലും സോഫ്‌റ്റ്‌വെയറിലോ നിലവിലുള്ള ഒരു അവതരണമോ പ്രോഗ്രാമോ എങ്ങനെ തുറക്കാനാകും?";
+window["i-e-q-en"+i] = "How can an existing presentation or program be opened in MS PowerPoint or in any other software?";
+window["i-e-A"+i] = "Ctrl + P";
+window["i-e-B"+i] = "Ctrl +  O";
+window["i-e-C"+i] = "Ctrl +  S";
+window["i-e-D"+i] = "Ctrl + Shift + O";
+window["i-e-ans"+i] = 2;
+
+
+i_eTotal = i;
